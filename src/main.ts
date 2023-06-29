cec043e8a70cc7cac89ef11047228341bc63522e
Álvaro Rubén Hurtado Maldonado <a.hurtado.bo@gmail.com>
2023-06-29 15:44:35 -0400

    Added build from frontend

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Enable CORS
+  app.use(express.static(join(__dirname, '..', 'front_dist')));
   await app.listen(3000);
 }
 bootstrap();
